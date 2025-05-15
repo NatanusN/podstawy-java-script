c3bd2ac1e6c9675970b1813fb6ea6ea004054b38
NatanusN <nataniel.ciuraszkiewicz@gmail.com>
2025-05-15 14:35:17 +0200

    dodano punktację

--- projekt_papier,kamień,nożyce.js
+++ projekt_papier,kamień,nożyce.js
@@ -1,51 +1,20 @@
+console.log("początek gry");
+const opcje = ['papier', 'kamień', 'nożyce'];
+let WybórGracza = null
 let punktyGracza = 0
 let punktyKomputera = 0
 document.querySelectorAll('.opcje').forEach(el => {
   el.addEventListener('click', function() {
-    const WybórGracza = this.dataset.choice;
+     WybórGracza = this.dataset.choice;
     console.log('Gracz wybrał:', WybórGracza);
     game();
   });
 });
-function game (){const WybórGracza = this.dataset;
-const opcje = ['papier', 'kamień', 'nożyce'];
+function game (){
+
 const wybórKomputera = opcje[Math.floor(Math.random() * opcje.length)];
 console.log('Komputer wybrał:', wybórKomputera);
-if (WybórGracza === wybórKomputera) {
-  alert('Remis');
-}
 let gra = '';
-
-if (
-  (WybórGracza === 'papier' && wybórKomputera === 'kamień')||
-  (WybórGracza === 'kamień' && wybórKomputera === 'nożyce')||
-  (WybórGracza === 'nożyce' && wybórKomputera === 'papier')
-) {
-  gra = 'wygrana';
-} else if (WybórGracza !== wybórKomputera) {
-  gra = 'przegrana';
-}
-if (gra === 'wygrana') {
-  punktyGracza++;
-} else if (gra === 'przegrana') {
-  punktyKomputera++;
-}
-if (gra === 'wygrana') {
-  alert('Zdobyłeś punkt');
-} else if (gra === 'przegrana'){
-  alert('Straciłeś punkt');
-} else {
-  alert('Remis');
-}
-if (punktyGracza === 3 || punktyKomputera === 3) {
-  if (punktyGracza === 3) {
-     alert('Wygrałeś!');
-    } else {
-    alert('Przegrałeś.');
-    }
-    punktyGracza = 0;
-    punktyKomputera = 0;
-}
     setTimeout(() => {
       let gra = '';
 
