427ffb230f734665f0d412588fef10b0387f5312
NatanusN <nataniel.ciuraszkiewicz@gmail.com>
2025-05-15 14:02:54 +0200

    następna przeszkoda

--- projekt_papier,kamień,nożyce.js
+++ projekt_papier,kamień,nożyce.js
@@ -1,3 +1,5 @@
+let punktyGracza = 0
+let punktyKomputera = 0
 document.querySelectorAll('.opcje').forEach(el => {
   el.addEventListener('click', function() {
     const WybórGracza = this.dataset.choice;
@@ -5,7 +7,7 @@ document.querySelectorAll('.opcje').forEach(el => {
   });
 });
 const WybórGracza = this.dataset;
-const opcje = ['papier', 'kamien', 'nożyce'];
+const opcje = ['papier', 'kamień', 'nożyce'];
 const wybórKomputera = opcje[Math.floor(Math.random() * opcje.length)];
 console.log('Komputer wybrał:', wybórKomputera);
 if (WybórGracza === wybórKomputera) {
@@ -22,3 +24,57 @@ if (
 } else if (WybórGracza !== wybórKomputera) {
   gra = 'przegrana';
 }
+if (gra === 'wygrana') {
+  punktyGracza++;
+} else if (gra === 'przegrana') {
+  punktyKomputera++;
+}
+if (gra === 'wygrana') {
+  alert('Zdobyłeś punkt');
+} else if (gra === 'przegrana'){
+  alert('Straciłeś punkt');
+} else {
+  alert('Remis');
+}
+if (punktyGracza === 3 || punktyKomputera === 3) {
+  if (punktyGracza === 3) {
+     alert('Wygrałeś!');
+    } else {
+    alert('Przegrałeś.');
+    }
+    punktyGracza = 0;
+    punktyKomputera = 0;
+}
+    setTimeout(() => {
+      let gra = '';
+
+      if (WybórGracza === wybórKomputera) {
+        gra = 'remis';
+      } else if (
+        (WybórGracza === 'papier' && wybórKomputera === 'kamień') ||
+        (WybórGracza === 'kamień' && wybórKomputera === 'nożyce') ||
+        (WybórGracza === 'nożyce' && wybórKomputera === 'papier')
+      ) {
+        gra = 'wygrana';
+      } else {
+        gra = 'przegrana';
+      }
+      if (gra === 'wygrana') {
+        punktyGracza++;
+        alert('Zdobyles punkt!');
+      } else if (gra === 'przegrana') {
+        punktyKomputera++;
+        alert('Straciłeś punkt.');
+      } else {
+        alert('Remis.');
+      }
+      if (punktyGracza === 3 || punktyKomputera === 3) {
+        if (punktyGracza === 3) {
+          alert('Wygrałeś!');
+        } else {
+          alert('Przegrałeś.');
+        }
+        punktyGracza = 0;
+        punktyKomputera = 0;
+      }
+    }, 1000);
