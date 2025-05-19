88e1f2817a6f4817d9f78fe52acc1d371868c1b7
NatanusN <nataniel.ciuraszkiewicz@gmail.com>
2025-05-19 09:05:39 +0200

    początek projektu quiz

---
+++ projekt_quiz.js
@@ -0,0 +1,17 @@
+const questions = [
+  {
+    question: "Z czego składa się woda?",
+    answers: ["Z CO2 i H", "Z Cl i O", "Z H2 i O", "Z N i O"],
+    correct: "Z H2 i O"
+  },
+  {
+    question: "Ile to 2+2*2?",
+    answers: ["4", "6", "8", "9"],
+    correct: "6"
+  },
+  {
+    question: "Kto jest autorem książki ''Pan Tadeusz''?",
+    answers: ["Jan Kochanowski", "Aleksander Fredro", "Juliusz Słowacki", "Adam Mickiewicz"],
+    correct: "Adam Mickiewicz"
+  }
+];
